2a23f764d86a0e97ed60923449dde0ddd1788069
garetshough14 <garetshough@gmail.com>
2024-06-19 14:31:21 -0700

    commented out unused links atm

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,8 +4,8 @@ function NavBar() {
   return (
     <div className='nav-container'>
         <div>Home</div>
-        <div>About</div>
-        <div>Team</div>
+        {/* <div>About</div>
+        <div>Team</div> */}
     </div>
   )
 }
